3e1530bb1601314192011752b47a814158d668cb
Roman lesyo <ladiginscormag@gmail.com>
2023-03-13 18:11:45 +0200

    bag fix

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import AuthForm from '../AuthForm/AuthForm';
+import Container from '../container/Container';
 import styles from './HomePage.module.css';
 
 function HomePage() {
   return (
-    <>
+    <Container>
       <AuthForm />
-    </>
+    </Container>
   );
 }
 
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FormContainer from './container.styled';
+
+const Container = ({ children }) => {
+  return <FormContainer>{children}</FormContainer>;
+};
+
+export default Container;
